36d14f3a115050960bb083936fce8e8b9eb8196b
Bruno Macedo <contato@brunomacedo.com.br>
2018-03-02 20:36:11 -0300

    [PYTHON] Create python server

--- python/run.py
+++ python/run.py
@@ -4,19 +4,20 @@ import socketserver
 import getpass
 
 class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
-  def log_message(self, format, *args):
-  logging.info("%s - - [%s] %s\n" % (
-  self.client_address[0],
-  self.log_date_time_string(),
-  format%args))
+	def log_message(self, format, *args):
+		logging.info("%s - - [%s] %s\n"% (
+			self.client_address[0],
+			self.log_date_time_string(),
+			format%args
+		))
 
 logging.basicConfig(
-  filename='/log/http-server.log',
-  format='%(asctime)s - %(levelname)s - %(message)s',
-  level=logging.INFO)
+	filename='/log/http-server.log',
+	format='%(asctime)s - %(levelname)s - %(message)s',
+	level=logging.INFO
+)
 
 logging.getLogger().addHandler(logging.StreamHandler())
-
 logging.info('inicializando...')
 PORT = 8000
 
