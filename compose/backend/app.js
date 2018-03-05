e26b4bce96d8c8a650dceb31061869a84e84219d
Bruno Macedo <contato@brunomacedo.com.br>
2018-03-05 20:42:37 -0300

    [Backend] Create app express server

--- compose/backend/app.js
+++ compose/backend/app.js
@@ -2,10 +2,33 @@ const express = require('express');
 const restful = require('node-restful');
 const server = express();
 const mongoose = restful.mongoose;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://database/mydb');
 
-server.get('/', (req, res) => res.send('Backend'));
+// server.get('/', (req, res) => res.send('Backend'));
+
+// Middlewares
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+server.use(cors());
+
+// ODM - Maping objects
+const Client = restful.model('Client', {
+  name: { type: String, required: true }
+});
+
+// Rest API -> From restful.mongoose
+Client.methods(['get', 'post', 'put', 'delete']);
+
+// Validate methods
+Client.updateOptions({new: true, runValidators: true});
+
+// Routes
+Client.register(server, '/clients');
+
+// Start server
 server.listen(3000);
