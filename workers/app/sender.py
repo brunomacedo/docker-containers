ad808fb6a04fdf50a5f410d9fee3b87ae86db35e
Bruno Macedo <contato@brunomacedo.com.br>
2018-03-08 23:45:02 -0300

    [PYTHON] Create script send email

---
+++ workers/app/sender.py
@@ -0,0 +1,12 @@
+from bottle import route, run, request
+
+@route('/', method='POST')
+def send():
+    subject = request.forms.get('subject')
+    message = request.forms.get('message')
+    return 'Mensagem enfileirada! Assunto: {} Mensagem {}'.format(
+        subject, message
+    )
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080, debug=True)
