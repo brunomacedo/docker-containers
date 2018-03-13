5520f124f4e6f97e281d9b5d3834b953444e89f4
Bruno Macedo <contato@brunomacedo.com.br>
2018-03-13 18:41:24 -0300

    [Velocity]

---
+++ velocity/template-ssl/run.bat
@@ -0,0 +1,3 @@
+
+ECHO Starting TCL on port %1 and ssl %2
+java -Dlogback.configurationFile=./config/logback.xml -Dfile.encoding=ISO-8859-1 -Duser.country=BR -Duser.language=pt -Dtemplatecache.local.update.enable=true -Dtemplatecache.replace.content.whitespace=true -jar server.jar %1 %2
