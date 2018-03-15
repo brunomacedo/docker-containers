bdef4736b9a9a1c3e821833a082ba2ad71155d1b
Bruno Macedo <contato@brunomacedo.com.br>
2018-03-14 23:55:57 -0300

    [APP] Create postgres connection

--- workers/app/sender.py
+++ workers/app/sender.py
@@ -1,9 +1,25 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = 'dbname=email_sender user=postgres host=database'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+def register_message(subject, message):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    cur.execute(SQL, (subject, message))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Message has been recorded!')
 
 @route('/', method='POST')
 def send():
     subject = request.forms.get('subject')
     message = request.forms.get('message')
+
+    register_message(subject, message)
     return 'Mensagem enfileirada! Assunto: {} Mensagem {}'.format(
         subject, message
     )
