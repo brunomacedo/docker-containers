f8ef0de692a769548cc8217cf4d1e8b9b893da42
Bruno Macedo <contato@brunomacedo.com.br>
2018-03-16 17:42:28 -0300

    Change

---
+++ velocity/templatecache/run.bat
@@ -0,0 +1,1 @@
+java -Dlogback.configurationFile=./config/logback.xml -Dfile.encoding=ISO-8859-1 -Duser.country=BR -Duser.language=pt -Dtemplatecache.local.update.enable=true -Dtemplatecache.replace.content.whitespace=true -jar server.jar %1
